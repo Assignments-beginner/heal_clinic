90374534ae5c949bc1d8b655b30f3e79cf855844
avishekataur <avishekataur@gmail.com>
2021-10-18 11:04:28 +0600

    firebase-init-done

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAkAdLUAUPSJs-HbvcM6hcuaVAwAiSxNM0",
+  authDomain: "health-care-999.firebaseapp.com",
+  projectId: "health-care-999",
+  storageBucket: "health-care-999.appspot.com",
+  messagingSenderId: "689909597088",
+  appId: "1:689909597088:web:55dffc0664142e2b0c1ac5",
+};
+
+export default firebaseConfig;
