9386caab7b2d61642ae3f3141afa76413e59531e
avishekataur <avishekataur@gmail.com>
2021-10-19 15:23:04 +0600

    treatment-L-R-done

--- src/Components/Treatment/Treatment.js
+++ src/Components/Treatment/Treatment.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-
+import "./Treatment.css";
 const Treatment = () => {
   return (
     <div style={{ backgroundColor: "#F9FBFF" }} className="pb-5">
@@ -10,21 +10,82 @@ const Treatment = () => {
       >
         check out more treatments from our doctors
       </h2>
-      <Container className="px-5">
-        {/* Treatments */}
+
+      {/* Treatments Right */}
+      <Container className="mb-5 p-5 bg-white shadow radius">
         <div className="d-flex align-items-center">
-
           <div>
+            <div className="pb-5">
               <h3>Lorem ipsum dolor sit amet.</h3>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque provident eum rerum totam error reiciendis minus voluptatem mollitia voluptatum excepturi ipsa ad odit consequuntur possimus illo, incidunt alias ipsam inventore.</p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Doloremque provident eum rerum totam error reiciendis minus
+                voluptatem mollitia voluptatum excepturi ipsa ad odit
+                consequuntur possimus illo, incidunt alias ipsam inventore.
+              </p>
+            </div>
+            <div>
+              <strong>By</strong> &nbsp;{" "}
+              <img
+                width="32"
+                height="32"
+                className="rounded-circle"
+                src="https://randomuser.me/api/portraits/men/80.jpg"
+                alt=""
+              />{" "}
+              <strong>Dr. Lorem</strong>
+            </div>
           </div>
-            <img width="360" height="245" src="https://media.istockphoto.com/photos/physical-doctor-consulting-with-patient-knee-problems-physical-picture-id1136089250?b=1&k=20&m=1136089250&s=170667a&w=0&h=Y8SzTM3U3doNltULm1--C1xaUcoK8FDvG0rdajKIjLs=" alt="" />
           <div>
-
+          <img
+            width="360"
+            height="245"
+            className="radius"
+            src="https://media.istockphoto.com/photos/physical-doctor-consulting-with-patient-knee-problems-physical-picture-id1136089250?b=1&k=20&m=1136089250&s=170667a&w=0&h=Y8SzTM3U3doNltULm1--C1xaUcoK8FDvG0rdajKIjLs="
+            alt=""
+          />
           </div>
-
         </div>
       </Container>
+      
+      {/* Treatments Left */}
+      <Container className="mb-5 p-5 bg-white shadow radius">
+        <div className="d-flex align-items-center">
+        <div>
+            <img
+            width="360"
+            height="245"
+            className="radius"
+            src="https://media.istockphoto.com/photos/physical-doctor-consulting-with-patient-knee-problems-physical-picture-id1136089250?b=1&k=20&m=1136089250&s=170667a&w=0&h=Y8SzTM3U3doNltULm1--C1xaUcoK8FDvG0rdajKIjLs="
+            alt=""
+          /></div>
+          <div className="ms-5">
+            <div className="pb-5">
+              <h3>Lorem ipsum dolor sit amet.</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Doloremque provident eum rerum totam error reiciendis minus
+                voluptatem mollitia voluptatum excepturi ipsa ad odit
+                consequuntur possimus illo, incidunt alias ipsam inventore.
+              </p>
+            </div>
+            <div>
+              <strong>By</strong> &nbsp;{" "}
+              <img
+                width="32"
+                height="32"
+                className="rounded-circle"
+                src="https://randomuser.me/api/portraits/men/80.jpg"
+                alt=""
+              />{" "}
+              <strong>Dr. Lorem</strong>
+            </div>
+          </div>
+          
+          
+        </div>
+      </Container>
+      
     </div>
   );
 };
